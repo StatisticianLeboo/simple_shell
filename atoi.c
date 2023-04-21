65cbf2b44f24f6961afe96c30d29d81f8447629f
StatisticianLeboo <lebooian61@gmail.com>
2023-04-21 16:24:46 +0300

    environ and error

---
+++ atoi.c
@@ -0,0 +1,76 @@
+#include "shell.h"
+
+/**
+ * interactive - checks if shell is running in interactive mode
+ * @info: pointer to struct containing information about the shell
+ *
+ * Return: 1 if running in interactive mode, 0 otherwise
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - checks if a character is a delimiter
+ * @c: character to check
+ * @delim: string of delimiter characters
+ * 
+ * Return: 1 if c is a delimiter, 0 otherwise
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim != '\0')
+	{
+		if (*delim == c)
+			return (1);
+		delim++;
+	}
+	return (0);
+}
+
+/**
+ * _isalpha - checks if a character is alphabetic
+ * @c: character to check
+ *
+ * Return: 1 if c is alphabetic, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to convert
+ *
+ * Return: converted integer or 0 if no numbers in string
+ */
+int _atoi(char *s)
+{
+	int sign = 1, output = 0;
+	unsigned int result = 0;
+	int flag = 0; /* 0 = no numbers found yet, 1 = numbers found, 2 = finished */
+
+	for (int i = 0; s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
+
---
+++ builtin.c
@@ -0,0 +1,107 @@
+#include "shell.h"
+
+/**
+ * _myexit - exits the shell
+ * @info: structure containing potential arguments
+ *
+ * This function exits the shell with a given exit status, provided as an argument
+ * to the "exit" command. If no argument is provided, the function exits with a
+ * default status of -1.
+ *
+ * Return: -2 if the "exit" command was found, or 0 otherwise.
+ */
+int _myexit(info_t *info)
+{
+	if (info->argv[1]) /* if there is an argument */
+	{
+		int exit_code = _erratoi(info->argv[1]);
+		if (exit_code == -1)
+		{
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return 1;
+		}
+		info->err_num = exit_code;
+	}
+	else
+	{
+		info->err_num = -1;
+	}
+
+	return -2;
+}
+
+/**
+ * _mycd - changes the current directory of the process
+ * @info: structure containing potential arguments
+ *
+ * This function changes the current working directory of the shell process, as
+ * specified by the "cd" command. If no argument is provided, the function changes
+ * the working directory to the user's home directory. If the argument is a hyphen
+ * "-", the function changes the working directory to the previous working directory
+ * (as stored in the OLDPWD environment variable). If the argument is a valid path,
+ * the function changes the working directory to that path.
+ *
+ * Return: Always 0.
+ */
+int _mycd(info_t *info)
+{
+	char *dir, buffer[1024];
+
+	if (!getcwd(buffer, 1024))
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+
+	if (!info->argv[1])
+	{
+		dir = _getenv(info, "HOME=");
+		if (!dir)
+			dir = _getenv(info, "PWD=");
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!_getenv(info, "OLDPWD="))
+		{
+			_puts(buffer);
+			_putchar('\n');
+			return 1;
+		}
+		dir = _getenv(info, "OLDPWD=");
+	}
+	else
+	{
+		dir = info->argv[1];
+	}
+
+	if (chdir(dir) == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]);
+		_eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+
+	return 0;
+}
+
+/**
+ * _myhelp - prints help information
+ * @info: structure containing potential arguments
+ *
+ * This function prints information about the available commands in the shell.
+ *
+ * Return: Always 0.
+ */
+int _myhelp(info_t *info)
+{
+	(void)info; /* avoid "unused parameter" warning */
+
+	_puts("help call works. Function not yet implemented\n");
+	return 0;
+}
+
